e2a0c7cde0252a6b893e19c6506d6e45baa5d7fc
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2022-03-22 20:15:44 -0600

    Add web assembly files and connect with the html

---
+++ pkg/puzzle_checker.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} index
+* @param {string} guess
+* @returns {boolean}
+*/
+export function check(index: number, guess: string): boolean;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly check: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
